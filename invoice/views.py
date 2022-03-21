dfbf8234b21ba9d7c0727228ea86729f4073dc57
Awadh-Awadh <awadhawadh84@yahoo.com>
2022-03-21 12:51:19 +0300

    add invoice form

--- invoice/views.py
+++ invoice/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
+from .forms import AddCompanyForm
 
 # Create your views here.
 
 
 
 def home(request):
+  if request.method == "POST":
+    form = AddCompanyForm(request.POST)
+    if form.is_valid():
+      form.save()
+  else:
+    form = AddCompanyForm()
 
-  return render(request, "home.html")
+  context = {
+    "form":form
+  }
+
+
+  return render(request, "home.html", context)
