3b720ebe98a538de554d597981e3b5fc50bbe788
Awadh-Awadh <awadhawadh84@yahoo.com>
2022-03-21 11:46:36 +0300

    add templates

--- invoice/admin.py
+++ invoice/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CompanyInvoice, AddItems
 
 # Register your models here.
+models = [CompanyInvoice, AddItems]
+
+for model in models:
+  admin.site.register(model)
--- invoice/models.py
+++ invoice/models.py
@@ -7,7 +7,7 @@ import uuid
 class CompanyInvoice(models.Model):
     company_name = models.CharField(max_length=255)
     invoice_date = models.DateField()
-    balance_due = models.IntegerField(max_length=255)
+    balance_due = models.IntegerField()
     address =  models.CharField(max_length=255)
 
     @property
@@ -15,10 +15,19 @@ class CompanyInvoice(models.Model):
        uid = str(uuid.uuid1())
        return uid.replace("-", "")[:12]
 
+    def __str__(self) -> str:
+        return self.company_name
+
 
 class AddItems(models.Model):
     item = models.CharField(max_length=255)
     descrition = models.TextField()
-    unit_cost = models.DecimalField(decimal_places=40)
+    unit_cost = models.DecimalField(decimal_places=2, max_digits=40)
     quantity = models.CharField(max_length=255)
     Company_invoice = models.ForeignKey(CompanyInvoice, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "AddItem"
+
+    def __str__(self):
+        return self.item
